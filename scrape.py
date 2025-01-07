72ac3cd121db6e0b7a2d241beead80073ac1ea1b
mapaulanavarro <mariapaula1018.navarro@gmail.com>
2025-01-07 11:37:06 -0700

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,61 +1,48 @@
-from selenium.webdriver.chrome.service import Remote, ChromeOptions
+from selenium.webdriver import Remote, ChromeOptions
 from selenium.webdriver.chromium.remote_connection import ChromiumRemoteConnection
 from bs4 import BeautifulSoup
-import streamlit as st
-import os
+from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate
 
-SBR_WEBDRIVER = 'brd-customer-hl_ff81bf84-zone-ai_mapaula_scraper:mzmixte3n2h7'
-   
+load_dotenv()
+
+SBR_WEBDRIVER = 'brd-customer-hl_ff81bf84-zone-ai_mapaula_scraper:mzmixte3n2h7@brd.superproxy.io:9515'
+
 def scrape_website(website):
-    print("Connecting to the website...")
-
-    sbr_connection = ChromiumRemoteConnection(SBR_WEBDRIVER, "goog", "chrome")
-    with Remote(sbr_connection,options=ChromeOptions()) as driver:
-        print('Connected! Navigating to the website...')
-        driver.get('website')
-        #Captcha
-        print('Waiting for the Captcha to be solved...')
-        solve_res = driver.execute('executeCdpCommand', 
-        {
-            'cmd': 'Captcha.waitForSolve', 
-            'params': {'detectTimeout':10000},
-        })
-        print ('Captcha solved:', solve_res['result']['status'])
-        print ('Navigated! Scraping the website...')
+    print('Connecting to Scraping Browser...')
+    sbr_connection = ChromiumRemoteConnection(SBR_WEBDRIVER, 'goog', 'chrome')
+    with Remote(sbr_connection, options=ChromeOptions()) as driver:
+        print('Connected! Navigating...')
+        driver.get(website)
+        print('Navigated! Scraping page content...')
         html = driver.page_source
-        print (html)
-        return(html)
+        print('Scraped! Closing connection...')
+        return (html)
 
 def extract_body_content(html_content):
-    print("Extracting the body content...")
-    soup = BeautifulSoup(html_content, 'html.parser')
+    soup = BeautifulSoup(html_content, "html.parser")
     body_content = soup.body
     if body_content:
         return str(body_content)
-    return "No body content found!"
+    return ""
 
-def clean_body_conent(body_content):
-    print("Cleaning the body content...")
-    soup = BeautifulSoup(body_content, 'html.parser')
+
+def clean_body_content(body_content):
+    soup = BeautifulSoup(body_content, "html.parser")
+
     for script_or_style in soup(["script", "style"]):
         script_or_style.extract()
 
+    # Get text or further process the content
     cleaned_content = soup.get_text(separator="\n")
-    cleaned_content = "\n".join(line.strip() for line in cleaned_content.splitlines() if line.strip()
+    cleaned_content = "\n".join(
+        line.strip() for line in cleaned_content.splitlines() if line.strip()
     )
+
     return cleaned_content
 
-def split_dom_content(dom_content, max_lenght =6000):
+
+def split_dom_content(dom_content, max_length=6000):
     return [
-        dom_content[i:i + max_lenght] for i in range(0, len(dom_content), max_lenght)
+        dom_content[i : i + max_length] for i in range(0, len(dom_content), max_length)
     ]
-    
-if "dom_content" in st.session_state.dom_content:
-    parse_description = st.txt_area("Descrbe what you want to parse?")
-
-    if st.button("Parse Content"):
-        if parse_description:
-            st.write("Parsing the DOM content")
-
-            dom_chunks = split_dom_content(st.session_state.dom_content)
-
